32509ac4bb90b066b1df2e76d97bd32d2ff1557d
Bradley Dover <32021942+bldover@users.noreply.github.com>
2018-09-07 18:15:01 -0400

    Removed weird symbols from lines 72 and 73

--- Documents/Georgia Tech/Fall 2018/cs2340/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ Documents/Georgia Tech/Fall 2018/cs2340/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -69,8 +69,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("gburdell3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Rafael Gandra");^M
-                  toPrint += p4.toString("rgandra3");^M
+                  Person4 p4 = new Person4("Rafael Gandra");
+                  toPrint += p4.toString("rgandra3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Avani Pavuluri");
